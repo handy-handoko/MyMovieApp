f923c95840a242f56d2b29ba5b18de5291266fc2
Luqman Fanani <luqman.fanani@gmail.com>
2017-07-28 08:52:50 +0800

    add authority and uri on contract

--- app/src/main/java/com/digitalnusantarastudio/mymovieapp/MovieContract.java
+++ app/src/main/java/com/digitalnusantarastudio/mymovieapp/MovieContract.java
@@ -1,5 +1,6 @@
 package com.digitalnusantarastudio.mymovieapp;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,7 +10,19 @@ import android.provider.BaseColumns;
 public class MovieContract {
     private MovieContract() {}
 
+    // The authority, which is how your code knows which Content Provider to access. just copypaste from manifest
+    public static final String AUTHORITY = "com.digitalnusantarastudio.mymovieapp";
+
+    // The base content URI = "content://" + <authority>
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    // Movies directory
+    public static final String PATH_MOVIES = "movies";
+
     public static final class MovieEntry implements BaseColumns{
+        // MovieEntry content URI = base content URI + path
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
+
         public static final String TABLE_NAME = "movie";
         public static final String COLLUMN_TITLE = "title";
         public static final String COLLUMN_POSTER_IMAGE_NAME = "poster_image_name";
