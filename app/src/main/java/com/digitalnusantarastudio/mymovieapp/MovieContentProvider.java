43b3145f740a327dcdfee9cce65856f14decf585
Luqman Fanani <luqman.fanani@gmail.com>
2017-07-28 08:51:26 +0800

    add content provider

---
+++ app/src/main/java/com/digitalnusantarastudio/mymovieapp/MovieContentProvider.java
@@ -0,0 +1,87 @@
+package com.digitalnusantarastudio.mymovieapp;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by luqman on 28/07/17.
+ */
+
+public class MovieContentProvider extends ContentProvider {
+    private MovieDbHelper mMovieDbHelper;
+
+    private final static int MOVIES = 100;
+    private final static int MOVIE_WITH_ID = 101;
+
+    private final static UriMatcher sUriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher(){
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES, MOVIES);
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES + "/#", MOVIE_WITH_ID);
+
+        return uriMatcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mMovieDbHelper = new MovieDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        final SQLiteDatabase db = mMovieDbHelper.getWritableDatabase();
+
+        int matcher = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (matcher) {
+            case MOVIES:
+                long id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+                if ( id > 0 ) {
+                    returnUri = ContentUris.withAppendedId(MovieContract.MovieEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return returnUri;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
