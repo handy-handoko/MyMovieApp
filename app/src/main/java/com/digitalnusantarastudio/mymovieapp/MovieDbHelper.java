e564cb98667e0eb8dddda3c46f829cc262bca16e
Luqman Fanani <luqman.fanani@gmail.com>
2017-07-28 08:52:17 +0800

    fix db helper bug

--- app/src/main/java/com/digitalnusantarastudio/mymovieapp/MovieDbHelper.java
+++ app/src/main/java/com/digitalnusantarastudio/mymovieapp/MovieDbHelper.java
@@ -22,18 +22,18 @@ public class MovieDbHelper extends SQLiteOpenHelper {
         final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " +
                 MovieContract.MovieEntry.TABLE_NAME + " (" +
                 MovieContract.MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                MovieContract.MovieEntry.COLLUMN_TITLE + "TEXT NOT NULL, " +
-                MovieContract.MovieEntry.COLLUMN_POSTER_IMAGE_NAME + "TEXT NOT NULL, " +
-                MovieContract.MovieEntry.COLLUMN_SYNOPSIS + "TEXT NOT NULL, " +
-                MovieContract.MovieEntry.COLLUMN_RATING + "FLOAT NOT NULL, " +
-                MovieContract.MovieEntry.COLLUMN_RELEASE_DATE + "TEXT NOT NULL " +
+                MovieContract.MovieEntry.COLLUMN_TITLE + " TEXT NOT NULL, " +
+                MovieContract.MovieEntry.COLLUMN_POSTER_IMAGE_NAME + " TEXT NOT NULL, " +
+                MovieContract.MovieEntry.COLLUMN_SYNOPSIS + " TEXT NOT NULL, " +
+                MovieContract.MovieEntry.COLLUMN_RATING + " FLOAT NOT NULL, " +
+                MovieContract.MovieEntry.COLLUMN_RELEASE_DATE + " TEXT NOT NULL " +
                 ");";
         db.execSQL(SQL_CREATE_MOVIE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXIST " + MovieContract.MovieEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + MovieContract.MovieEntry.TABLE_NAME);
         onCreate(db);
     }
 
